ae0b3f70a1acb6f36a9b686711021ee3ff354265
Tom Sharkey <tbsharkey@gmail.com>
2019-08-14 03:49:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-word.match(/^[aeiouAEIOU]/) ? true:false
+word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +11,9 @@ text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].+\.$/) ? true:false
+text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.scan(/\d/).length == 10 ? true:false
+phone.scan(/\d/).length == 10 ? true : false
 end
